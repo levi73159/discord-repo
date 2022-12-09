89404437586f7634911ff6cbb57c45bc004977b1
levi73159 <88604538+levi73159@users.noreply.github.com>
2022-12-09 17:03:21 -0600

    Revert "keep_alive"
    This reverts commit 65ad4eabc59609fd30bb6ee140946403e3c3b55d.

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from threading import Thread
 import time
 import json
 import os
-import keep_alive
 
 
 intents = discord.Intents.default()
@@ -170,5 +169,5 @@ async def on_reaction_remove(reaction: discord.Reaction, user: discord.User):
     elif reaction.emoji == '⬇':
         add_paycheck(user.id, 1)
 
-keep_alive.keep_alive()
+
 client.run(os.getenv('KEY'))
